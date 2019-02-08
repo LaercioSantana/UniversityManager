61165df4ce0eaeba62e59186a5de9a61618b0800
Laercio Santana <laerciosantana021@hotmail.com>
2019-02-07 23:39:21 -0300

    add EF config and initial models

---
+++ UniversityManager/DAL/SchoolContext.cs
@@ -0,0 +1,23 @@
+﻿using UniversityManager.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace UniversityManager.DAL
+{
+    public class SchoolContext : DbContext
+    {
+
+        public SchoolContext() : base("SchoolContext")
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ UniversityManager/Models/Subject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UniversityManager.Models
+{
+    public class Subject
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int SubjectID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
